97f3fe77b89cc9ab969f8fe7db474347e6fbb9fd
Siddhesh Ghadi <sghadi1203@gmail.com>
2025-06-09 16:25:26 +0530

    Add missing metadata in bundle
    Signed-off-by: Siddhesh Ghadi <sghadi1203@gmail.com>

--- gitops-operator-bundle/patch-bundle.py
+++ gitops-operator-bundle/patch-bundle.py
@@ -56,8 +56,10 @@ with open(CONFIG_FILE, 'r') as file:
 package = "openshift-gitops-operator"
 version = config['release']['version'] 
 channel = "latest,gitops-1.16"
-skip_range = ">=1.0.0 <1.16.0"
+#skip_range = ">=1.0.0 <1.16.0"
+skip_range = "" # empty for z-stream
 replaces = "openshift-gitops-operator.v1.16.1"
+argocd_version = "2.14.13"
 
 relatedImages = [
     {'name': 'manager', 'image': images['operator']},
@@ -118,6 +120,7 @@ if 'labels' not in csv['metadata']:
     csv['metadata']['labels'] = {}
 
 csv['metadata']['annotations']['operators.openshift.io/valid-subscription'] = '["OpenShift Container Platform", "OpenShift Platform Plus"]'
+csv['metadata']['annotations']['operators.operatorframework.io/internal-objects'] = '["gitopsservices.pipelines.openshift.io"]'
 csv['metadata']['annotations']['features.operators.openshift.io/fips-compliant'] = 'true'
 csv['metadata']['annotations']['features.operators.openshift.io/cnf'] = 'false'
 csv['metadata']['annotations']['features.operators.openshift.io/cni'] = 'false'
@@ -134,6 +137,8 @@ csv['metadata']['name'] = f"{package}.v{version}"
 csv['metadata']['annotations']['olm.skipRange'] = skip_range
 csv['spec']['version'] = version
 csv['spec']['replaces'] = replaces
+
+csv['spec']['description'] = f"Red Hat OpenShift GitOps is a declarative continuous delivery platform based on [Argo CD](https://argoproj.github.io/argo-cd/). It enables teams to adopt GitOps principles for managing cluster configurations and automating secure and repeatable application delivery across hybrid multi-cluster Kubernetes environments. Following GitOps and infrastructure as code principles, you can store the configuration of clusters and applications in Git repositories and use Git workflows to roll them out to the target clusters.\n\n## Features\n* Automated install and upgrades of Argo CD\n* Manual and automated configuration sync from Git repositories to target OpenShift and Kubernetes clusters\n* Support for the Helm and Kustomize templating tools\n* Configuration drift detection and visualization on live clusters\n* Audit trails of rollouts to the clusters\n* Monitoring and logging integration with OpenShift\n##Components\n* Argo CD {argocd_version}\n\n## How to Install \nAfter installing the OpenShift GitOps operator, an instance  of Argo CD is installed in the `openshift-gitops` namespace which has sufficent privileges for managing cluster configurations. You can create additional Argo CD instances using the `ArgoCD` custom resource within the desired namespaces.\n```yaml\napiVersion: argoproj.io/v1beta1\nkind: ArgoCD\nmetadata:\n  name: argocd\nspec:\n  server:\n    route:\n      enabled: true\n```\n\nOpenShift GitOps is a layered product on top of OpenShift that enables teams to adopt GitOps principles for managing cluster configurations and automating secure and repeatable application delivery across hybrid multi-cluster Kubernetes environments. OpenShift GitOps is built around Argo CD as the core upstream project and assists customers to establish an end-to-end application delivery workflow on GitOps principles.\n"
 
 # image replacements
 csv['metadata']['annotations']['containerImage'] = images['operator']
