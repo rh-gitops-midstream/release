19c9fb83a6c716a036210f65f339c2367aa28fed
Siddhesh Ghadi <sghadi1203@gmail.com>
2025-09-24 08:57:52 +0530

    Add debug statements
    Signed-off-by: Siddhesh Ghadi <sghadi1203@gmail.com>

--- rpms/microshift-gitops/generate-source-tarball.sh
+++ rpms/microshift-gitops/generate-source-tarball.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+pwd
+ls -l
+
 echo "Running tarball script"
 
-tar -czvf ./rpm/microshift/argocd-sources.tar.gz -C ./sources argo-cd
+tar -czvf ./rpm/microshift-gitops/argocd-sources.tar.gz -C ./sources argo-cd
 
-ls -l ./rpm/microshift/
+ls -l ./rpm/microshift-gitops/
