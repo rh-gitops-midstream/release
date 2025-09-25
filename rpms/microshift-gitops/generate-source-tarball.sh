8207c132918a9c0b3450ce2aabdfa12204580854
Siddhesh Ghadi <sghadi1203@gmail.com>
2025-09-25 11:09:42 +0530

    Revert "fix script"
    This reverts commit 5110680703912e4024d5837c0e375f10a4c02a63.

--- rpms/microshift-gitops/generate-source-tarball.sh
+++ rpms/microshift-gitops/generate-source-tarball.sh
@@ -3,10 +3,10 @@ set -euxo pipefail
 
 echo "Running tarball script"
 
-#tar -czvf ./rpms/microshift-gitops/argo-cd-sources.tar.gz -C ./sources argo-cd
+#tar -czvf ./rpms/microshift-gitops/argocd-sources.tar.gz -C ./sources argo-cd
 
 #ls -l ./rpms/microshift-gitops/
 
 mv ./rpms/microshift-gitops/microshift-gitops.spec .
-tar -czvf argo-cd-sources.tar.gz -C ./sources argo-cd
+tar -czvf argocd-sources.tar.gz -C ./sources argo-cd
 rm -rf ./sources
